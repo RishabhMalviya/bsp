1e62848e31bf381be1b342d19fd5189f9ab79eb6
Rishabh Malviya <rishabh.malviya@gmail.com>
2026-05-26 16:21:41 -0700

    move tensors to CPU during episode collection to save time

--- src/bsp/pretraining/trainer.py
+++ src/bsp/pretraining/trainer.py
@@ -111,7 +111,7 @@ class BodySchemaTrainer(BaseTrainer):
 		return obs, actions
 
 	def _collect_episodes(self, env: gym.Env) -> None:
-		# self.agent.to_cpu()  # Saves on moving tensors back and forth at every timestep
+		self.agent.to_cpu()  # Saves on moving tensors back and forth at every timestep
 
 		obs, _ = env.reset(seed=self.cfg.seed)
 		for _ in range(self.cfg.curiosity_pre_training.num_collections_per_loop):
@@ -128,8 +128,9 @@ class BodySchemaTrainer(BaseTrainer):
 				self.timestep += 1
 
 			self.collected_episodes += 1
-		
-		# self.agent.to_device()
+			self.logger.log({'Collected Episodes': self.collected_episodes}, step=self.timestep)
+
+		self.agent.to_device()
 
 	def _train_dynamics_predictor(self) -> None:
 		for _ in range(self.cfg.curiosity_pre_training.dynamics_training_iterations):
